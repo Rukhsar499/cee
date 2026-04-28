d660d0ee24c1382a4c1e9c9a04f8429b297a3f38
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2026-04-28 10:42:15 +0530

    hii

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const slider = document.getElementById("slider");
+const container = document.querySelector(".compare-container");
+const afterImg = document.querySelector(".compare-img.after");
+
+let isDragging = false;
+
+slider.addEventListener("mousedown", () => isDragging = true);
+window.addEventListener("mouseup", () => isDragging = false);
+
+window.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+
+    let rect = container.getBoundingClientRect();
+    let offsetX = e.clientX - rect.left;
+
+    if (offsetX < 0) offsetX = 0;
+    if (offsetX > rect.width) offsetX = rect.width;
+
+    let percent = (offsetX / rect.width) * 100;
+
+    afterImg.style.width = percent + "%";
+    slider.style.left = percent + "%";
+});
